03ee93bb53ed0fc2aca087e3a8dc79b73ae4ad0c
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-08 19:28:03 +0530

    form handling with json placeholder server

--- src/API/FunSignUp.js
+++ src/API/FunSignUp.js
@@ -1,62 +1,93 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Form } from 'react-bootstrap'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function FunSignUp() {
-
     const [validated, setValidated] = useState(false);
-    const [sign, setSign] = useState({
-        first_name: "",
-        last_name: "",
-        username: "",
-        email: "",
-        password: ""
-    })
+
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const getFormData = (event) => {
         const form = event.currentTarget;
-        console.log(sign.first_name, sign.last_name, sign.username, sign.email, sign.password,);
-
         if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
         }
         setValidated(true);
-
     }
     const handleChange = (e) => {
         e.preventDefault();
-    }
-
-    const onChange = (e) => {
-        setSign({ ...sign, [e.target.name]: e.target.value })
+        if (firstname, lastname, username, email, password) {
+            toast.success('Your data is successful', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            fetch('http://localhost:3000/fan', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ firstname, lastname, username, email, password })
+            })
+                .then((result) => {
+                    result.json().then((response) => {
+                        console.log(response);
+                    })
+                })
+        }
+        else {
+            toast.error('Please required your Message', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+        setFirstName("");
+        setLastName("");
+        setUsername("");
+        setEmail("");
+        setPassword("");
     }
 
     return <section>
         <Container>
             <Row>
-                <Col lg={9} md={8} className="mx-auto">
+                <Col lg={9} md={9} className="mx-auto">
                     <div className="form-tab">
-                        <h2 className='heading'>Create Your Fan Account</h2>
+                        <h1 className='heading'>React <span>&</span> Form</h1>
                         <div className="form-sec">
                             <Form noValidate validated={validated} onSubmit={getFormData}>
                                 <Form.Group className="mb-3">
                                     <Form.Label>First Name *</Form.Label>
-                                    <Form.Control type="text" placeholder='FirstName' value={sign.first_name} onChange={onChange} name="first_name" />
+                                    <Form.Control type="text" placeholder='FirstName' value={firstname} onChange={(e) => setFirstName(e.target.value)} required />
                                 </Form.Group>
                                 <Form.Group className="mb-3">
                                     <Form.Label>LastName *</Form.Label>
-                                    <Form.Control type="text" placeholder='LastName' value={sign.last_name} onChange={onChange} name="last_name" />
+                                    <Form.Control type="text" placeholder='LastName' value={lastname} onChange={(e) => setLastName(e.target.value)} required />
                                 </Form.Group>
                                 <Form.Group className="mb-3">
                                     <Form.Label>UserName</Form.Label>
-                                    <Form.Control type="text" placeholder='userName' value={sign.username} onChange={onChange} name="username" />
+                                    <Form.Control type="text" placeholder='userName' value={username} onChange={(e) => setUsername(e.target.value)} required />
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formBasicEmail" >
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="email" value={sign.email} onChange={onChange} name="email" />
+                                    <Form.Control type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" value={sign.password} onChange={onChange} name="password" />
+                                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
                                 </Form.Group>
                                 <Form.Group className='check-sec' controlId="formBasicCheckbox">
                                     <Form.Check type="checkbox" label=" I accept the " />
@@ -64,9 +95,8 @@ export default function FunSignUp() {
                                 </Form.Group>
                                 <Form.Group className="mt-4 text-center">
                                     <button className="btn" type="submit" onClick={handleChange}>Submit</button>
-                                    {/* <ToastContainer /> */}
+                                    <ToastContainer />
                                 </Form.Group>
-
                             </Form>
                         </div>
                     </div>
--- src/API/TelentSignUp.js
+++ src/API/TelentSignUp.js
@@ -1,79 +1,107 @@
-
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Container, Row, Col, Form } from 'react-bootstrap'
-export default function TelentSignUp() {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+export default function TelentSignUp() {
     const [validated, setValidated] = useState(false);
-    const [login, setLogin] = useState({
-        first_name: "",
-        last_name: "",
-        username: "",
-        email: "",
-        password: ""
-    })
+
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const getFormData = (event) => {
         const form = event.currentTarget;
-        console.log(login.first_name, login.last_name, login.username, login.email, login.password,);
-
         if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
         }
         setValidated(true);
-
     }
     const handleChange = (e) => {
         e.preventDefault();
-
+        if (firstname, lastname, username, email, password) {
+            toast.success('Your data is successful', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            fetch('http://localhost:3000/talent', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ firstname, lastname, username, email, password })
+            })
+                .then((result) => {
+                    result.json().then((response) => {
+                        console.log(response);
+                    })
+                })
+        }
+        else {
+            toast.error('Please required your Message', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+        setFirstName("");
+        setLastName("");
+        setUsername("");
+        setEmail("");
+        setPassword("");
     }
 
-    const onChange = (e) => {
-        setLogin({ ...login, [e.target.name]: e.target.value })
-    }
-    return (
-        <section>
-            <Container>
-                <Row>
-                    <Col lg={9} md={8} className="mx-auto">
-                        <div className="form-tab">
-                            <h2 className='heading'>Create Your Fan Account</h2>
-                            <div className="form-sec">
-                                <Form noValidate validated={validated} onSubmit={getFormData}>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label>First Name *</Form.Label>
-                                        <Form.Control type="text" placeholder='FirstName' value={login.first_name} onChange={onChange} name="first_name" />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label>LastName *</Form.Label>
-                                        <Form.Control type="text" placeholder='LastName' value={login.last_name} onChange={onChange} name="last_name" />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label>UserName</Form.Label>
-                                        <Form.Control type="text" placeholder='userName' value={login.username} onChange={onChange} name="username" />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3" controlId="formBasicEmail" >
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control type="email" placeholder="email" value={login.email} onChange={onChange} name="email" />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" value={login.password} onChange={onChange} name="password" />
-                                    </Form.Group>
-                                    <Form.Group className='check-sec' controlId="formBasicCheckbox">
-                                        <Form.Check type="checkbox" label=" I accept the " />
-                                        <a href="/">Terms and Conditions</a>
-                                    </Form.Group>
-                                    <Form.Group className="mt-4 text-center">
-                                        <button className="btn" type="submit" onClick={handleChange}>Submit</button>
-                                        {/* <ToastContainer /> */}
-                                    </Form.Group>
-
-                                </Form>
-                            </div>
+    return <section>
+        <Container>
+            <Row>
+                <Col lg={9} md={9} className="mx-auto">
+                    <div className="form-tab">
+                        <h1 className='heading'>React <span>&</span> Form</h1>
+                        <div className="form-sec">
+                            <Form noValidate validated={validated} onSubmit={getFormData}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>First Name *</Form.Label>
+                                    <Form.Control type="text" placeholder='FirstName' value={firstname} onChange={(e) => setFirstName(e.target.value)} required />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>LastName *</Form.Label>
+                                    <Form.Control type="text" placeholder='LastName' value={lastname} onChange={(e) => setLastName(e.target.value)} required />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>UserName</Form.Label>
+                                    <Form.Control type="text" placeholder='userName' value={username} onChange={(e) => setUsername(e.target.value)} required />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicEmail" >
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+                                </Form.Group>
+                                <Form.Group className='check-sec' controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label=" I accept the " />
+                                    <a href="/">Terms and Conditions</a>
+                                </Form.Group>
+                                <Form.Group className="mt-4 text-center">
+                                    <button className="btn" type="submit" onClick={handleChange}>Submit</button>
+                                    <ToastContainer />
+                                </Form.Group>
+                            </Form>
                         </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
 }
